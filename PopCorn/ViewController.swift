34881ba1ca840a1b80a9a15bd65497b908e3aec9
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-04-13 13:12:45 +0200

    First counter

--- PopCorn/ViewController.swift
+++ PopCorn/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monLabel: UILabel!
+    var monAge : Int = 0
+    var monNom : String = "Carl"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
     }
 
+    private func setupView(){
+        monLabel.text = "Je m'appel " + monNom + " et j'ai " + String(monAge) + "ans"
+        //myimageview.Image = UIImage(named: "icon_caddy_off")
+    }
 
+    @IBAction func myButtonDidTouch(_ sender: UIButton) {
+        monAge += 1
+        monLabel.text = "Je m'appel " + monNom + " et j'ai " + String(monAge) + "ans"
+    }
 }
 
