1a61bcec49f5e11fe1d25cfce5d55dc6dff28261
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-04-26 11:02:39 +0200

    Button YT working

--- PopCorn/ViewController.swift
+++ PopCorn/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var monLabel: UILabel!
     var monAge : Int = 0
     var monNom : String = "Carl"
+    var urlBA : String = "https://www.youtube.com/watch?v=YIx4nbTSV_Q"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,13 @@ class ViewController: UIViewController {
        
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        print("test");
+        if let url = URL(string: urlBA) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
 
     
 }
